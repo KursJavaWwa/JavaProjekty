9db799ac6cd906c99a395a77a57c7aab789567f7
[KrzysztofKaczyński] <qris70@gmail.com>
2018-04-22 19:16:29 +0200

    Ramka1

---
+++ Udemy/Dokumentowanie/src/dokumentowanie/Main.java
@@ -0,0 +1,17 @@
+package dokumentowanie;
+
+/**
+ * @author Arkadiusz Włodarczyk
+ * @version 1.0.4.2 06/08/2015
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Pracownik p = new Pracownik("Arkadiusz", "Włodarczyk");
+
+    }
+
+}
--- Udemy/Jframe/src/ukladgraficzny/Main.java
+++ Udemy/Jframe/src/ukladgraficzny/Main.java
@@ -1,4 +1,21 @@
 package ukladgraficzny;
 
-public class Main {
+import javax.swing.*;
+
+public class Main extends JFrame {
+
+    public Main() {
+
+        super("Układ graficzne (Layout)");
+        this.setBounds(300, 320, 300, 200);
+
+
+        this.setDefaultCloseOperation(3);
+
+    }
+
+    public static void main(String[] args) {
+        new Main().setVisible(true);
+    }
 }
+
