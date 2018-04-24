e4be4dd0ff6e9a70615156139df59eee839e7e72
[KrzysztofKaczyński] <qris70@gmail.com>
2018-04-24 12:19:13 +0200

    Zdarzenia start

---
+++ Udemy/Zdarzenia/src/zdarzenia/Main.java
@@ -0,0 +1,25 @@
+package zdarzenia;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame
+{
+    public Main()
+    {
+      super("Zdarzenia");
+      this.setBounds(300, 300, 300, 200);
+
+      initComponents();
+
+      this.setDefaultCloseOperation(3);
+    }
+    public void initComponents()
+    {
+
+    }
+    public static void main(String[] args)
+    {
+
+    }
+}
