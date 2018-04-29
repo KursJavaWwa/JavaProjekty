00180582d3956b3029893dbb32af799268dcdfd5
[KrzysztofKaczyński] <qris70@gmail.com>
2018-04-29 20:04:51 +0200

    Zadanie 1.1

---
+++ Udemy/APPswing/src/app1/App1.java
@@ -0,0 +1,12 @@
+package app1;
+
+import javax.swing.*;
+
+public class App1 {
+
+    private JButton button1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
